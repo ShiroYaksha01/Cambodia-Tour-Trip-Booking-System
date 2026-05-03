9bd7b427648d04e0cfc1ee73f9a65995733fb73c
Toek <toektak77@gmail.com>
2026-05-03 19:17:29 +0700

    try to fix bug

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,14 +1,14 @@
-<template>
-  <router-view />
-</template>
-
-
 <!-- <template>
+  <router-view />
+</template> -->
+
+
+<template>
   <CustomerHomepage />
 </template>
 
 <script setup lang="ts">
 // Import the CustomerDashboardView component
 import CustomerHomepage from './views/CustomerHomepage.vue'
-</script> -->
+</script>
 
