3d3ab17a6cf0692e5952e9be190612a59737e16d
ShiroYaksha01 <chansethatek@gmail.com>
2026-06-17 00:45:39 +0700

    fix: Add missing computed import in ProviderDetail.vue

--- frontend/src/views/customer/ProviderDetail.vue
+++ frontend/src/views/customer/ProviderDetail.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
+import { ref, onMounted, computed } from "vue";
 import { useRoute, useRouter } from "vue-router";
 import CustomerNavbar from "../../components/customer/CustomerNavbar.vue";
 import CustomerFooter from "../../components/customer/CustomerFooter.vue";
